06b8b4eb3e67fc8a4a716521d19defb9d853c859
Mikhail Smirnov <smirnov-mn@mail.ru>
2021-10-07 13:32:29 +0300

    Initial Commit

---
+++ Pull To Refresh Custom/Pull_To_Refresh_CustomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pull_To_Refresh_CustomApp.swift
+//  Pull To Refresh Custom
+//
+//  Created by Mikhail Smirnov on 07.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct Pull_To_Refresh_CustomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
